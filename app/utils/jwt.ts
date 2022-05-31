225e3361a5b46a582fa15c44382030ddfa7ca8d3
Junmin Ahn <junminahn@outlook.com>
2022-05-31 16:02:00 -0700

    chore: add bceid service endpoint

--- app/utils/jwt.ts
+++ app/utils/jwt.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import jwt from 'jsonwebtoken';
+import jws from 'jws';
+import jwkToPem from 'jwk-to-pem';
+import axios from 'axios';
 import getConfig from 'next/config';
 
 const { serverRuntimeConfig = {} } = getConfig() || {};
-const { jwt_secret } = serverRuntimeConfig;
+const { jwt_secret, idir_jwks_uri, idir_issuer } = serverRuntimeConfig;
 
 type Data = {
   success: boolean;
@@ -18,4 +21,28 @@ export const validateRequest = async (req: NextApiRequest, res: NextApiResponse<
     console.error(error);
     return null;
   }
+};
+
+export const getIdirUserGuid = async (token: string) => {
+  const { header } = jws.decode(token);
+
+  const { keys }: any = await axios.get(idir_jwks_uri).then((res) => res.data);
+
+  const key = keys.find((jwkKey: any) => jwkKey.kid === header.kid);
+  const isValidKid = !!key;
+
+  if (!isValidKid) return null;
+
+  const pem = jwkToPem(key);
+
+  const { identity_provider, idir_userid }: any = jwt.verify(token, pem, {
+    audience: 'sso-requests',
+    issuer: idir_issuer,
+    maxAge: '8h',
+    ignoreExpiration: true,
+  });
+
+  if (identity_provider !== 'idir') return null;
+
+  return idir_userid;
 };
