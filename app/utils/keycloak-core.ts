1b66fea1622d9bb9958b85fa6574169085f41242
Junmin Ahn <junminahn@outlook.com>
2022-11-22 20:07:18 -0800

    chore: set timeout 1s to keycloak api

--- app/utils/keycloak-core.ts
+++ app/utils/keycloak-core.ts
@@ -53,7 +53,7 @@ class KeycloakCore {
       realmName: 'master',
       requestConfig: {
         /* Axios request config options https://github.com/axios/axios#request-config */
-        timeout: 2000,
+        timeout: 1000,
       },
     });
 
