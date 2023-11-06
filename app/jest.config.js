b1777e70c9d9abb9dc2bd61eacd80db0807a89ce
jonathan langlois <jonathan.langlois@gov.bc.ca>
2023-11-06 14:44:54 -0800

    test: unit tests
    ad djest config for unit tests

---
+++ app/jest.config.js
@@ -0,0 +1,18 @@
+const nextJest = require('next/jest.js')
+ 
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+ 
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+  moduleDirectories: ['<rootDir>', 'node_modules']
+}
+ 
+module.exports = createJestConfig(config)
