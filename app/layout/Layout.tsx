0ad0683e3e1bd7f0d15e92b862aababfda38a8ae
Nithin Shekar Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2023-11-27 09:41:37 -0800

    feat: end user session when access token expires (#135)
    * feat: logout user when refresh and access tokens expire
    
    * feat: refactored to logout user when refresh token expires
    
    * feat: updated session type

--- app/layout/Layout.tsx
+++ app/layout/Layout.tsx
@@ -4,13 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCommentDots, faEnvelope, faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import Button from '@button-inc/bcgov-theme/Button';
 import Footer from '@button-inc/bcgov-theme/Footer';
-import StyledLink from '@button-inc/bcgov-theme/Link';
 import styled from 'styled-components';
-import { startCase } from 'lodash';
 import BCSans from './BCSans';
 import Navigation from './Navigation';
 import BottomAlertProvider from './BottomAlert';
-import { useSession } from 'next-auth/react';
+import { getSession, useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 import { User } from 'next-auth';
 
@@ -160,9 +158,25 @@ const RightMenuItems = () => (
 // identity_provider, idir_userid, client_roles, family_name, given_name
 function Layout({ children, onLoginClick, onLogoutClick }: any) {
   const router = useRouter();
-  const { data } = useSession();
-  const currentUser: Partial<User> = data?.user!;
+  const session = useSession();
+  const currentUser: Partial<User> = session?.data?.user!;
   const pathname = router.pathname;
+
+  const checkSession = async () => {
+    if (Date.now() > session?.data?.accessTokenExpiry) {
+      const session = await getSession();
+      if (session?.error === 'RefreshAccessTokenError') {
+        onLogoutClick();
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      const interval = setInterval(checkSession, 1000 * 1);
+      return () => clearInterval(interval);
+    }
+  });
 
   const rightSide = currentUser ? (
     <LoggedUser>
