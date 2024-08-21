c336ee7122537175dcd62fafad97386e7ffe7f33
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2024-08-21 14:29:42 -0700

    fix: restore nextjs headers

--- app/next.config.js
+++ app/next.config.js
@@ -64,53 +64,53 @@ module.exports = {
     if (process.env.SECURE_HEADERS === 'false') return [];
 
     return [
-      // {
-      //   source: '/(.*)',
-      //   headers: [
-      //     {
-      //       key: 'Content-Security-Policy',
-      //       value:
-      //         "default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
-      //     },
-      //     {
-      //       key: 'X-Frame-Options',
-      //       value: 'DENY',
-      //     },
-      //     {
-      //       key: 'X-Content-Type-Options',
-      //       value: 'nosniff',
-      //     },
-      //     {
-      //       key: 'Referrer-Policy',
-      //       value: 'no-referrer',
-      //     },
-      //     {
-      //       key: 'Permissions-Policy',
-      //       value:
-      //         'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()',
-      //     },
-      //     {
-      //       key: 'X-XSS-Protection',
-      //       value: '1; mode=block',
-      //     },
-      //     {
-      //       key: 'Cache-Control',
-      //       value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-      //     },
-      //     {
-      //       key: 'Pragma',
-      //       value: 'no-cache',
-      //     },
-      //     {
-      //       key: 'Expires',
-      //       value: '0',
-      //     },
-      //     {
-      //       key: 'Surrogate-Control',
-      //       value: 'no-store',
-      //     },
-      //   ],
-      // },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+          {
+            key: 'Surrogate-Control',
+            value: 'no-store',
+          },
+        ],
+      },
     ];
   },
   poweredByHeader: false,
